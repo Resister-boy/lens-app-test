df751d859fce8bb57e07d56fb35bc27e9dc2fe14
Resister-boy <hassanpumped17@gmail.com>
2022-08-20 18:03:32 +0900

    프로젝트 세팅 & lens API 연동

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: [
+      "lens.infura-ipfs.io",
+      "icon-library.com",
+    ]
+  },
 }
 
 module.exports = nextConfig
